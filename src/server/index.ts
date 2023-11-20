087863c3f6aa212278fe607b28b74f0e693aa024
Alex Luckett <alexgluckett@gmail.com>
2023-11-20 15:25:39 +0000

    add cdp error handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -12,6 +12,8 @@ import { configureRateLimitPlugin } from "./plugins/rateLimit";
 import { configureBlankiePlugin } from "./plugins/blankie";
 import { configureCrumbPlugin } from "./plugins/crumb";
 import { configureInitialiseSessionPlugin } from "server/plugins/initialiseSession/configurePlugin";
+import { catchAll } from './helpers/errors'
+
 
 import pluginLocale from "./plugins/locale";
 import pluginSession from "./plugins/session";
@@ -113,33 +115,36 @@ async function createServer(routeConfig: RouteConfig) {
     AddressService,
   ]);
 
-  server.ext(
-    "onPreResponse",
-    (request: HapiRequest, h: HapiResponseToolkit) => {
-      const { response } = request;
+  // TODO re-enable
+  // server.ext(
+  //   "onPreResponse",
+  //   (request: HapiRequest, h: HapiResponseToolkit) => {
+  //     const { response } = request;
+  //
+  //     if ("isBoom" in response && response.isBoom) {
+  //       return h.continue;
+  //     }
+  //
+  //     if ("header" in response && response.header) {
+  //       response.header("X-Robots-Tag", "noindex, nofollow");
+  //
+  //       const WEBFONT_EXTENSIONS = /\.(?:eot|ttf|woff|svg|woff2)$/i;
+  //       if (!WEBFONT_EXTENSIONS.test(request.url.toString())) {
+  //         response.header(
+  //           "cache-control",
+  //           "private, no-cache, no-store, must-revalidate, max-age=0"
+  //         );
+  //         response.header("pragma", "no-cache");
+  //         response.header("expires", "0");
+  //       } else {
+  //         response.header("cache-control", "public, max-age=604800, immutable");
+  //       }
+  //     }
+  //     return h.continue;
+  //   }
+  // );
 
-      if ("isBoom" in response && response.isBoom) {
-        return h.continue;
-      }
-
-      if ("header" in response && response.header) {
-        response.header("X-Robots-Tag", "noindex, nofollow");
-
-        const WEBFONT_EXTENSIONS = /\.(?:eot|ttf|woff|svg|woff2)$/i;
-        if (!WEBFONT_EXTENSIONS.test(request.url.toString())) {
-          response.header(
-            "cache-control",
-            "private, no-cache, no-store, must-revalidate, max-age=0"
-          );
-          response.header("pragma", "no-cache");
-          response.header("expires", "0");
-        } else {
-          response.header("cache-control", "public, max-age=604800, immutable");
-        }
-      }
-      return h.continue;
-    }
-  );
+  server.ext('onPreResponse', catchAll);
 
   server.ext("onRequest", (request: HapiRequest, h: HapiResponseToolkit) => {
     const { pathname } = getRequestInfo(request);
