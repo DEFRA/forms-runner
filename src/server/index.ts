63fa3ee6028f9922dca6419a703d8cbb8e12a06d
Alex Luckett <alexgluckett@gmail.com>
2023-11-23 15:31:12 +0000

    Revert "dump config out for debugging - this is test code so nothing sensitive to keep"
    This reverts commit 671b86951f05dcafd9a6344d1c25383a8df852fe.

--- src/server/index.ts
+++ src/server/index.ts
@@ -173,9 +173,6 @@ async function createServer(routeConfig: RouteConfig) {
     encoding: "base64json",
   });
 
-  server.logger.info("Config loaded");
-  server.logger.info(config);
-
   return server;
 }
 
