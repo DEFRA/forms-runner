9dd37d36f677e3221644df8a08a208e286e61702
Alex Luckett <alexgluckett@gmail.com>
2023-12-07 16:58:31 +0000

    fix title and rebadge as alpha

--- config/default.js
+++ config/default.js
@@ -55,11 +55,11 @@ module.exports = {
    * Service
    */
   serviceUrl: "http://localhost:3009", //This is used for redirects back to the runner.
-  serviceName: "Digital Form Builder - Runner",
+  serviceName: "Defra Forms testing sandbox",
   serviceStartPage: "",
   privacyPolicyUrl: "",
   feedbackLink: "#", // Used in your phase banner. Can be a URL or more commonly mailto mailto:feedback@department.gov.uk
-  phaseTag: "beta", // Accepts "alpha" |"beta" | ""
+  phaseTag: "alpha", // Accepts "alpha" |"beta" | ""
 
   /**
    * Session storage
