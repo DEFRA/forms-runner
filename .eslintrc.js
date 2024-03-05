95b5523740b23797df1143250baeedfa7fd01fa1
Colin Rotherham <work@colinr.com>
2024-03-05 10:37:25 +0000

    Set up ESlint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+/**
+ * ESLint config
+ *
+ * @type {import('eslint').ESLint.ConfigData}
+ */
+module.exports = {
+  ignorePatterns: ['**/dist/**', '**/public/**', 'node_modules'],
+  overrides: [
+    {
+      extends: [
+        'standard',
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:promise/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+      ],
+      files: ['**/*.{cjs,js,mjs,ts}'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest'
+      },
+      plugins: ['@typescript-eslint', 'import', 'promise'],
+      rules: {
+        'import/namespace': [
+          'error',
+          {
+            allowComputed: true
+          }
+        ],
+        'no-unused-vars': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
+      },
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.cjs', '.js', '.mjs', '.ts']
+        },
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true
+          }
+        }
+      }
+    },
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
+  root: true
+}
