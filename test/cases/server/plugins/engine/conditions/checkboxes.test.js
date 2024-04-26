286367c05f98b5e968ce73591bfe4e32509ceb31
David Stone <davidjamesstone@gmail.com>
2024-04-26 10:26:55 +0100

    Add tests

---
+++ test/cases/server/plugins/engine/conditions/checkboxes.test.js
@@ -0,0 +1,78 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import createServer from '../../../../../../src/server/index.js'
+import FormData from 'form-data'
+
+const testDir = dirname(fileURLToPath(import.meta.url))
+const okStatusCode = 200
+const redirectStatusCode = 302
+const htmlContentType = 'text/html'
+const key = 'wqJmSf'
+
+describe('Checkboxes based conditions', () => {
+  let server
+
+  // Create server before each test
+  beforeAll(async () => {
+    server = await createServer({
+      formFileName: 'checkboxes.json',
+      formFilePath: testDir
+    })
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop()
+  })
+
+  test('Checkboxes are rendered', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/checkboxes/first-page'
+    })
+
+    expect(res.statusCode).toEqual(okStatusCode)
+    expect(res.headers['content-type']).toContain(htmlContentType)
+    expect(res.result).toContain(
+      `<input class="govuk-checkboxes__input" id="${key}" name="${key}" type="checkbox" value="shire">`
+    )
+    expect(res.result).toContain(
+      `<input class="govuk-checkboxes__input" id="${key}-2" name="${key}" type="checkbox" value="race">`
+    )
+    expect(res.result).toContain(
+      `<input class="govuk-checkboxes__input" id="${key}-3" name="${key}" type="checkbox" value="pantomime">`
+    )
+    expect(res.result).toContain(
+      `<input class="govuk-checkboxes__input" id="${key}-4" name="${key}" type="checkbox" value="other">`
+    )
+  })
+
+  test('Testing POST /checkboxes/first-page with nothing checked redirects correctly', async () => {
+    const form = new FormData()
+
+    const res = await server.inject({
+      method: 'POST',
+      url: '/checkboxes/first-page',
+      headers: form.getHeaders(),
+      payload: form.getBuffer()
+    })
+
+    expect(res.statusCode).toEqual(redirectStatusCode)
+    expect(res.headers.location).toBe('/checkboxes/second-page')
+  })
+
+  test('Testing POST /checkboxes/first-page with "other" checked redirects correctly', async () => {
+    const form = new FormData()
+    form.append(key, 'other')
+
+    const res = await server.inject({
+      method: 'POST',
+      url: '/checkboxes/first-page',
+      headers: form.getHeaders(),
+      payload: form.getBuffer()
+    })
+
+    expect(res.statusCode).toEqual(redirectStatusCode)
+    expect(res.headers.location).toBe('/checkboxes/third-page')
+  })
+})
---
+++ test/cases/server/plugins/engine/conditions/text.test.js
@@ -0,0 +1,84 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import createServer from '../../../../../../src/server/index.js'
+import FormData from 'form-data'
+
+const testDir = dirname(fileURLToPath(import.meta.url))
+const okStatusCode = 200
+const redirectStatusCode = 302
+const htmlContentType = 'text/html'
+const key = 'wqJmSf'
+
+describe('TextField based conditions', () => {
+  let server
+
+  // Create server before each test
+  beforeAll(async () => {
+    server = await createServer({
+      formFileName: 'text.json',
+      formFilePath: testDir
+    })
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop()
+  })
+
+  test('TextField is rendered', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/text/first-page'
+    })
+
+    expect(res.statusCode).toEqual(okStatusCode)
+    expect(res.headers['content-type']).toContain(htmlContentType)
+    expect(res.result).toContain(
+      `<input class="govuk-input govuk-input--width-20" id="${key}" name="${key}" type="text">`
+    )
+  })
+
+  test('Testing POST /text/first-page with an nothing string redirects correctly', async () => {
+    const form = new FormData()
+
+    const res = await server.inject({
+      method: 'POST',
+      url: '/text/first-page',
+      headers: form.getHeaders(),
+      payload: form.getBuffer()
+    })
+
+    expect(res.statusCode).toEqual(redirectStatusCode)
+    expect(res.headers.location).toBe('/text/second-page')
+  })
+
+  test('Testing POST /text/first-page with an empty string redirects correctly', async () => {
+    const form = new FormData()
+    form.append(key, '')
+
+    const res = await server.inject({
+      method: 'POST',
+      url: '/text/first-page',
+      headers: form.getHeaders(),
+      payload: form.getBuffer()
+    })
+
+    expect(res.statusCode).toEqual(redirectStatusCode)
+    expect(res.headers.location).toBe('/text/second-page')
+  })
+
+  test('Testing POST /text/first-page with an string "other" redirects correctly', async () => {
+    const form = new FormData()
+    form.append(key, 'other')
+
+    const res = await server.inject({
+      method: 'POST',
+      url: '/text/first-page',
+      headers: form.getHeaders(),
+      payload: form.getBuffer()
+    })
+
+    expect(res.statusCode).toEqual(redirectStatusCode)
+    expect(res.headers.location).toBe('/text/third-page')
+  })
+})
