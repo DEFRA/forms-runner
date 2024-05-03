3581e69c6df1ffe682b73c513d8c0add2bf8d9f5
Colin Rotherham <work@colinr.com>
2024-05-03 09:19:25 +0100

    Add ESLint rules and plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,26 +12,129 @@ module.exports = {
         'eslint:recommended',
         'plugin:import/recommended',
         'plugin:import/typescript',
+        'plugin:jsdoc/recommended',
+        'plugin:n/recommended',
         'plugin:promise/recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'prettier'
       ],
       files: ['**/*.{cjs,js,mjs,ts}'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        ecmaVersion: 'latest'
+        ecmaVersion: 'latest',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname
       },
-      plugins: ['@typescript-eslint', 'import', 'promise'],
+      plugins: ['@typescript-eslint', 'import', 'jsdoc', 'n', 'promise'],
       rules: {
-        'import/namespace': [
+        'no-console': 'error',
+
+        // Check type imports are identified
+        '@typescript-eslint/consistent-type-imports': [
           'error',
           {
-            allowComputed: true
+            fixStyle: 'inline-type-imports'
           }
         ],
+
+        // Allow void return shorthand in arrow functions
+        '@typescript-eslint/no-confusing-void-expression': [
+          'error',
+          {
+            ignoreArrowShorthand: true
+          }
+        ],
+
+        // Only show warnings for missing types
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+
+        // Check type support for template string implicit `.toString()`
+        '@typescript-eslint/restrict-template-expressions': [
+          'error',
+          {
+            allowBoolean: true,
+            allowNumber: true
+          }
+        ],
+
+        // Skip rules handled by TypeScript compiler
+        'import/default': 'off',
+        'import/extensions': 'off',
+        'import/named': 'off',
+        'import/namespace': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/no-unresolved': 'off',
+
+        // Check type imports are annotated inline
+        'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+
+        // Check import or require statements are A-Z ordered
+        'import/order': [
+          'error',
+          {
+            alphabetize: { order: 'asc' },
+            'newlines-between': 'always'
+          }
+        ],
+
+        // Check for valid formatting
+        'jsdoc/check-line-alignment': [
+          'warn',
+          'never',
+          {
+            tags: ['param', 'property', 'typedef', 'returns']
+          }
+        ],
+
+        // Require hyphens before param description
+        // Aligns with TSDoc style: https://tsdoc.org/pages/tags/param/
+        'jsdoc/require-hyphen-before-param-description': [
+          'warn',
+          'always',
+          {
+            tags: {
+              param: 'always',
+              property: 'always',
+              returns: 'never'
+            }
+          }
+        ],
+
+        // JSDoc blocks are optional but must be valid
+        'jsdoc/require-jsdoc': [
+          'error',
+          {
+            enableFixer: false,
+            require: {
+              FunctionDeclaration: false
+            }
+          }
+        ],
+
+        // JSDoc @param is optional
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/require-param-type': 'off',
+        'jsdoc/require-param': 'off',
+
+        // JSDoc @returns is optional
+        'jsdoc/require-returns-description': 'off',
+        'jsdoc/require-returns-type': 'off',
+        'jsdoc/require-returns': 'off',
+
+        // Skip rules handled by TypeScript compiler
+        'n/no-extraneous-require': 'off',
+        'n/no-extraneous-import': 'off',
+        'n/no-missing-require': 'off',
+        'n/no-missing-import': 'off',
+
+        // Prefer rules that are type aware
         'no-unused-vars': 'off',
         'no-use-before-define': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
       },
@@ -41,11 +144,35 @@ module.exports = {
         },
         'import/resolver': {
           node: true,
-          typescript: {
-            alwaysTryTypes: true,
-            project: ['./tsconfig.json']
-          }
+          typescript: true
         }
+      }
+    },
+    {
+      extends: ['plugin:jsdoc/recommended-typescript-flavor'],
+      files: ['**/*.{cjs,js,mjs}'],
+      plugins: ['jsdoc'],
+      rules: {
+        // JSDoc blocks are optional but must be valid
+        'jsdoc/require-jsdoc': [
+          'error',
+          {
+            enableFixer: false,
+            require: {
+              FunctionDeclaration: false
+            }
+          }
+        ],
+
+        // JSDoc @param types are mandatory for JavaScript
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/require-param-type': 'error',
+        'jsdoc/require-param': 'off',
+
+        // JSDoc @returns is optional
+        'jsdoc/require-returns-description': 'off',
+        'jsdoc/require-returns-type': 'off',
+        'jsdoc/require-returns': 'off'
       }
     },
     {
