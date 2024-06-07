7afd4195c9076c6d95c1dbda39f51e8b0b788f34
Alex Luckett <alexgluckett@gmail.com>
2024-06-07 11:51:00 +0100

    We should never generate a session cookie password. Remove the code.

--- src/server/plugins/session.ts
+++ src/server/plugins/session.ts
@@ -1,7 +1,6 @@
 import yar from '@hapi/yar'
 
 import config from '~/src/server/config.js'
-import generateCookiePassword from '~/src/server/utils/generateCookiePassword.js'
 
 export default {
   plugin: yar,
@@ -10,7 +9,7 @@ export default {
       expiresIn: config.sessionTimeout
     },
     cookieOptions: {
-      password: config.sessionCookiePassword || generateCookiePassword(),
+      password: config.sessionCookiePassword,
       isSecure: !!config.isDev,
       isHttpOnly: true,
       isSameSite: 'Lax'
--- src/server/utils/generateCookiePassword.ts
+++
@@ -1,7 +0,0 @@
-const generateCookiePassword = (): string =>
-  Array(32)
-    .fill(0)
-    .map(() => Math.random().toString(36).charAt(2))
-    .join('')
-
-export default generateCookiePassword
