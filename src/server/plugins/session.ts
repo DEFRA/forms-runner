7898a2a32a08771d2c26596cbe8c1cc2de80c505
Alex Luckett <alexgluckett@gmail.com>
2024-06-11 12:13:08 +0100

    enable storing of blank sessions

--- src/server/plugins/session.ts
+++ src/server/plugins/session.ts
@@ -2,19 +2,26 @@ import yar from '@hapi/yar'
 
 import config from '~/src/server/config.js'
 
+/**
+ * Yar is used for temporary session data but not form submissions, e.g. UI helpers, session flags.
+ */
 export default {
   plugin: yar,
   options: {
-    maxCookieSize: 0, // Always use server-side storage
     cache: {
       cache: 'session',
+      segment: 'session',
       expiresIn: config.sessionTimeout
     },
-    storeBlank: false,
+    /**
+     * @todo storeBlank is current commented out as it's a minor efficiency gain but breaks the auth tests if enabled.
+     * this only seems to affect the auth code, which we might remove anyway so it's temporarily disabled.
+     */
+    // storeBlank: false,
     errorOnCacheNotReady: true,
     cookieOptions: {
       password: config.sessionCookiePassword,
-      isSecure: config.isProd,
+      isSecure: !!config.isDev,
       isHttpOnly: true,
       isSameSite: 'Lax'
     }
