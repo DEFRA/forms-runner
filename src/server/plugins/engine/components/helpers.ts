65195863b92f149afd72083d3e96c420ded88a88
Colin Rotherham <work@colinr.com>
2024-07-11 12:14:50 +0100

    Remove build schema helpers

--- src/server/plugins/engine/components/helpers.ts
+++ src/server/plugins/engine/components/helpers.ts
@@ -1,6 +1,6 @@
 import { type ComponentDef } from '@defra/forms-model'
 import { add, startOfToday, sub } from 'date-fns'
-import joi from 'joi'
+import { type CustomHelpers } from 'joi'
 
 import * as Components from '~/src/server/plugins/engine/components/index.js'
 
@@ -40,82 +40,8 @@ export function getComponentField(component: ComponentDef) {
   return Components[type]
 }
 
-/**
- * FIXME:- this code is bonkers. buildFormSchema and buildState schema are duplicates.
- * The xxField classes should be responsible for generating their own schemas.
- */
-export function buildSchema(type, keys) {
-  const schema = type?.isJoi ? type : joi[type?.type ?? type]()
-
-  Object.keys(keys).forEach((key) => {
-    let val = keys[key]
-    if (key === 'regex') {
-      val = new RegExp(val)
-    }
-    schema[key](typeof val === 'boolean' ? undefined : val)
-  })
-
-  return schema
-}
-
-export function buildFormSchema(schemaType, component, isRequired = true) {
-  let schema = buildSchema(schemaType, component.schema)
-
-  if (isRequired) {
-    schema = schema.required()
-  }
-
-  if (component.title) {
-    schema = schema.label(component.title.toLowerCase())
-  }
-
-  if (component.options.required === false) {
-    schema = schema.allow(null, '').optional()
-  }
-
-  if (schema.trim && component.schema.trim !== false) {
-    schema = schema.trim()
-  }
-
-  return schema
-}
-
-export function buildStateSchema(schemaType, component) {
-  let schema = buildSchema(schemaType, component.schema)
-
-  if (component.title) {
-    schema = schema.label(component.title.toLowerCase())
-  }
-
-  if (component.options.required !== false) {
-    schema = schema.required()
-  }
-
-  if (component.options.required === false) {
-    schema = schema.allow(null, '').optional()
-  }
-
-  if (schema.trim && component.schema.trim !== false) {
-    schema = schema.trim()
-  }
-
-  return schema
-}
-
-export function getFormSchemaKeys(_name, schemaType, component) {
-  const schema = buildFormSchema(schemaType, component)
-
-  return { [component.name]: schema }
-}
-
-export function getStateSchemaKeys(name, schemaType, component) {
-  const schema = buildStateSchema(schemaType, component)
-
-  return { [name]: schema }
-}
-
 export const addClassOptionIfNone = (
-  options: { classes?: string; [prop: string]: any },
+  options: Extract<ComponentDef['options'], { classes?: string }>,
   className: string
 ) => {
   if (!options.classes) {
@@ -126,7 +52,7 @@ export function getCustomDateValidator(
   maxDaysInPast?: number,
   maxDaysInFuture?: number
 ) {
-  return (value: Date, helpers: joi.CustomHelpers) => {
+  return (value: Date, helpers: CustomHelpers) => {
     if (maxDaysInPast) {
       const minDate = sub(startOfToday(), { days: maxDaysInPast })
       if (value < minDate) {
