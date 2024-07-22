aa682b94c45ba0f33118acb3ac36af450d6402a4
Alex Luckett <alexgluckett@gmail.com>
2024-07-22 12:14:12 +0100

    Remove unused InitialiseSession types

--- src/server/schemas/types.ts
+++ src/server/schemas/types.ts
@@ -1,5 +1,3 @@
-import { type ConfirmationPage } from '@defra/forms-model'
-
 import { type DetailItem } from '~/src/server/plugins/engine/models/types.js'
 
 export interface Field {
@@ -13,19 +11,4 @@ export interface Question {
   category?: string
   question: string
   fields: Field[]
-}
-
-export type InitialiseSessionField = Pick<Field, 'key' | 'answer'>
-export type InitialiseSessionQuestion = {
-  fields: InitialiseSessionField[]
-} & Question
-
-export interface InitialiseSessionSchema {
-  options: {
-    callbackUrl: string
-    redirectPath?: string
-    customText: ConfirmationPage['customText']
-    components: ConfirmationPage['components']
-  }
-  questions: InitialiseSessionQuestion[]
 }
