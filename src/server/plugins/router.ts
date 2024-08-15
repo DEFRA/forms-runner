05b4a3bf59d1f31d35936fb6525d727af627c9fc
Alex Luckett <alexgluckett@gmail.com>
2024-08-15 11:28:28 +0100

    Add content for request-an-update-to-a-form

--- src/server/plugins/router.ts
+++ src/server/plugins/router.ts
@@ -23,7 +23,9 @@ const privacyPolicies: Record<string, string> = {
   'apply-for-a-county-parish-holding-cph-number':
     'https://www.gov.uk/guidance/rpa-privacy-notices',
   'submit-a-complaint-for-weeds-that-are-causing-damage-or-harm':
-    'https://www.gov.uk/government/organisations/natural-england/about/personal-information-charter'
+    'https://www.gov.uk/government/organisations/natural-england/about/personal-information-charter',
+  'request-an-update-to-a-form':
+    'https://defra.sharepoint.com/sites/Community4442/SitePages/Privacy-notice--Defra-Forms.aspx'
 }
 
 export default {
