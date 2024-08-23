ffac4d1de8de2586f8473d8043ed987bdf09605f
David Stone <davidjamesstone@gmail.com>
2024-08-23 13:35:28 +0100

    Remove logging

--- src/server/plugins/engine/services/formSubmissionService.js
+++ src/server/plugins/engine/services/formSubmissionService.js
@@ -1,9 +1,7 @@
 import { config } from '~/src/config/index.js'
-import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 import { postJson } from '~/src/server/services/httpService.js'
 
 const submissionUrl = config.get('submissionUrl')
-const logger = createLogger()
 
 /**
  * Initiates a CDP file upload
@@ -18,13 +16,9 @@ export async function persistFiles(files, persistedRetrievalKey) {
     persistedRetrievalKey
   }
 
-  logger.info(`Completed persistence request`)
-
   const result = await postJsonByType(`${submissionUrl}/files/persist`, {
     payload
   })
-
-  logger.info(`Completed persistence request`)
 
   return result
 }
