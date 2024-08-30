2f954244006b770d69db26b65eb00b2a29035bed
David Stone <davidjamesstone@gmail.com>
2024-08-30 14:55:45 +0100

    Remove test code

--- src/server/plugins/engine/services/formSubmissionService.js
+++ src/server/plugins/engine/services/formSubmissionService.js
@@ -1,7 +1,7 @@
 import { config } from '~/src/config/index.js'
 import { postJson } from '~/src/server/services/httpService.js'
 
-const submissionUrl = 'http://localhost:3002' // config.get('submissionUrl')
+const submissionUrl = config.get('submissionUrl')
 
 /**
  * Persist files by extending the time-to-live to 30 days
