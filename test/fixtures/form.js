51537819cac9a78425bf28bc1c010e4eb417a017
Colin Rotherham <work@colinr.com>
2024-10-09 21:25:05 +0100

    Consolidate form test fixtures

---
+++ test/fixtures/form.js
@@ -0,0 +1,86 @@
+import {
+  ComponentType,
+  ControllerPath,
+  ControllerType
+} from '@defra/forms-model'
+
+const now = new Date()
+
+/**
+ * @satisfies {FormMetadataAuthor}
+ */
+export const author = {
+  id: 'J6PlucvwkmNlYxX9HnSEj27AcJAVx_08IvZ-IPNTvAN',
+  displayName: 'Enrique Chase'
+}
+
+/**
+ * @satisfies {FormMetadataState}
+ */
+export const state = {
+  createdAt: now,
+  createdBy: author,
+  updatedAt: now,
+  updatedBy: author
+}
+
+/**
+ * @satisfies {FormMetadata}
+ */
+export const metadata = {
+  id: '661e4ca5039739ef2902b214',
+  slug: 'test-form',
+  title: 'Test form',
+  organisation: 'Defra',
+  teamName: 'Defra Forms',
+  teamEmail: 'defraforms@defra.gov.uk',
+  createdAt: now,
+  createdBy: author,
+  updatedAt: now,
+  updatedBy: author
+}
+
+/**
+ * @satisfies {FormDefinition}
+ */
+export const definition = {
+  name: '',
+  startPage: '/page-one',
+  pages: [
+    {
+      path: '/page-one',
+      title: 'Page one',
+      section: 'section',
+      components: [
+        {
+          type: ComponentType.TextField,
+          name: 'textField',
+          title: 'This is your first field',
+          hint: 'Help text',
+          options: {},
+          schema: {}
+        }
+      ],
+      next: [{ path: ControllerPath.Summary }]
+    },
+    {
+      title: 'Summary',
+      path: ControllerPath.Summary,
+      controller: ControllerType.Summary
+    }
+  ],
+  conditions: [],
+  sections: [
+    {
+      name: 'section',
+      title: 'Section title',
+      hideTitle: false
+    }
+  ],
+  lists: [],
+  outputEmail: 'enrique.chase@defra.gov.uk'
+}
+
+/**
+ * @import { FormDefinition, FormMetadata, FormMetadataAuthor, FormMetadataState } from '@defra/forms-model'
+ */
