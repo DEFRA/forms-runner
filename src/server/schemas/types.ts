df0b14e2ba7db08347077ff908457f28dc2f2d76
David Stone <davidjamesstone@gmail.com>
2024-10-16 11:33:33 +0100

    Update types to support repeaters and submission

--- src/server/schemas/types.ts
+++ src/server/schemas/types.ts
@@ -5,10 +5,39 @@ export interface Field {
   type: DetailItem['dataType']
   title: DetailItem['title']
   answer: DetailItem['rawValue']
+  item: DetailItem
 }
 
 export interface Question {
   category?: string
   question: string
   fields: Field[]
+}
+
+export interface SubmitRecord {
+  name: string
+  title: string
+  value: string
+}
+
+export interface SubmitRecordSet {
+  title: string
+  value: SubmitRecord[][]
+}
+
+export interface SubmitPayload {
+  sessionId: string
+  retrievalKey: string
+  main: SubmitRecord[]
+  repeaters: SubmitRecordSet[]
+}
+
+export interface SubmitResponsePayload {
+  message: string
+  result: {
+    files: {
+      main: string
+      repeaters: Record<string, string>
+    }
+  }
 }
