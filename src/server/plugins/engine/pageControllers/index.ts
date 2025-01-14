3f09552f8be457498bb3bcec11f795ae608488b8
David Stone <davidjamesstone@gmail.com>
2025-01-14 17:00:56 +0000

    Add support for TerminalPageController

--- src/server/plugins/engine/pageControllers/index.ts
+++ src/server/plugins/engine/pageControllers/index.ts
@@ -3,6 +3,7 @@ export {
   QuestionPageController, // Export alongside alias
   QuestionPageController as PageController
 } from '~/src/server/plugins/engine/pageControllers/QuestionPageController.js'
+export { TerminalPageController } from '~/src/server/plugins/engine/pageControllers/TerminalPageController.js'
 export { SummaryPageController } from '~/src/server/plugins/engine/pageControllers/SummaryPageController.js'
 export { StatusPageController } from '~/src/server/plugins/engine/pageControllers/StatusPageController.js'
 export { FileUploadPageController } from '~/src/server/plugins/engine/pageControllers/FileUploadPageController.js'
