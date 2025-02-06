fd2f8115d48a262f055fe25d584d0b802a410797
Alex Luckett <alexluckett@users.noreply.github.com>
2025-02-06 15:00:06 +0000

    Use subitem state instead of parent state for repeater output (#686)
    * Use subitem state instead of parent state for repeater output
    
    * Use a more readable var name

--- src/server/plugins/engine/outputFormatters/machine/v1.ts
+++ src/server/plugins/engine/outputFormatters/machine/v1.ts
@@ -91,16 +91,20 @@ function categoriseData(items: DetailItem[]) {
 function extractRepeaters(item: DetailItemRepeat) {
   const repeaters: Record<string, string>[] = []
 
-  item.subItems.forEach((subItem) => {
-    const repeaterEntry: Record<string, string> = {}
+  item.subItems.forEach((inputRepeaterItem) => {
+    const outputRepeaterItem: Record<string, string> = {}
 
-    subItem.forEach((subSubitem) => {
-      repeaterEntry[subSubitem.name] = getAnswer(subSubitem.field, item.state, {
-        format: 'data'
-      })
+    inputRepeaterItem.forEach((repeaterComponent) => {
+      outputRepeaterItem[repeaterComponent.name] = getAnswer(
+        repeaterComponent.field,
+        repeaterComponent.state,
+        {
+          format: 'data'
+        }
+      )
     })
 
-    repeaters.push(repeaterEntry)
+    repeaters.push(outputRepeaterItem)
   })
 
   return repeaters
