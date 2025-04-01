802bde14b6c41c9204f2670c5e66d6e98b7930b2
David Stone <davidjamesstone@gmail.com>
2025-04-01 15:45:11 +0100

    Sonar changes

--- src/server/plugins/engine/models/FormModel.ts
+++ src/server/plugins/engine/models/FormModel.ts
@@ -19,6 +19,7 @@ import { add } from 'date-fns'
 import { Parser, type Value } from 'expr-eval'
 import joi from 'joi'
 
+import { type ListFormComponent } from '~/src/server/plugins/engine/components/ListFormComponent.js'
 import {
   hasListFormField,
   type Component
@@ -319,71 +320,6 @@ export class FormModel {
     return context
   }
 
-  private pageStateIsInvalid(context: FormContext, page: PageControllerClass) {
-    // Get any list-bound fields on the page
-    const listFields = page.collection.fields.filter(hasListFormField)
-
-    for (const field of listFields) {
-      const list = field.list
-
-      // Filter out YesNo as they can't be conditional
-      if (list !== undefined && field.type !== ComponentType.YesNoField) {
-        const hasOptionalItems =
-          list.items.filter((item) => item.condition).length > 0
-
-        if (hasOptionalItems) {
-          const { evaluationState, state } = context
-
-          // For each list field that is bound to a list that contains any conditional items,
-          // we need to check any answers are still valid. Do this by evaluating the conditions
-          // and ensuring any current answers are all included in the set of valid answers
-          const validValues = list.items
-            .filter((item) =>
-              item.condition
-                ? this.conditions[item.condition]?.fn(evaluationState)
-                : true
-            )
-            .map((item) => item.value)
-
-          // Get the field state
-          const fieldState = field.getFormValueFromState(state)
-
-          if (fieldState !== undefined) {
-            let isInvalid = false
-            const isArray = Array.isArray(fieldState)
-
-            // Check if any saved state value(s) are still valid
-            // and return true if any are invalid
-            if (isArray) {
-              isInvalid = !fieldState.every((item) =>
-                validValues.includes(item)
-              )
-            } else {
-              isInvalid = !validValues.includes(fieldState)
-            }
-
-            if (isInvalid) {
-              if (!context.errors) {
-                context.errors = []
-              }
-
-              const text = `${field.title} contains ${isArray ? 'invalid values' : 'an invalid value'}`
-
-              context.errors.push({
-                text,
-                name: field.name,
-                href: `#${field.name}`,
-                path: [`#${field.name}`]
-              })
-            }
-
-            return isInvalid
-          }
-        }
-      }
-    }
-  }
-
   private initialiseContext(context: FormContext) {
     // For the V2 engine, we need to initialise `evaluationState` to null
     // for all keys. This is because the current condition evaluation
@@ -418,6 +354,77 @@ export class FormModel {
       if (typeof context.state[key] !== 'undefined') {
         context.relevantState[key] = context.state[key]
       }
+    }
+  }
+
+  private pageStateIsInvalid(context: FormContext, page: PageControllerClass) {
+    // Get any list-bound fields on the page
+    const listFields = page.collection.fields.filter(hasListFormField)
+
+    // For each list field that is bound to a list that contains any conditional items,
+    // we need to check any answers are still valid. Do this by evaluating the conditions
+    // and ensuring any current answers are all included in the set of valid answers
+    for (const field of listFields) {
+      const list = field.list
+
+      // Filter out YesNo as they can't be conditional
+      if (list !== undefined && field.type !== ComponentType.YesNoField) {
+        const hasOptionalItems =
+          list.items.filter((item) => item.condition).length > 0
+
+        if (hasOptionalItems) {
+          return this.fieldStateIsInvalid(context, field, list)
+        }
+      }
+    }
+  }
+
+  private fieldStateIsInvalid(
+    context: FormContext,
+    field: ListFormComponent,
+    list: List
+  ) {
+    const { evaluationState, state } = context
+
+    const validValues = list.items
+      .filter((item) =>
+        item.condition
+          ? this.conditions[item.condition]?.fn(evaluationState)
+          : true
+      )
+      .map((item) => item.value)
+
+    // Get the field state
+    const fieldState = field.getFormValueFromState(state)
+
+    if (fieldState !== undefined) {
+      let isInvalid = false
+      const isArray = Array.isArray(fieldState)
+
+      // Check if any saved state value(s) are still valid
+      // and return true if any are invalid
+      if (isArray) {
+        isInvalid = !fieldState.every((item) => validValues.includes(item))
+      } else {
+        isInvalid = !validValues.includes(fieldState)
+      }
+
+      if (isInvalid) {
+        if (!context.errors) {
+          context.errors = []
+        }
+
+        const text = `${field.title} contains ${isArray ? 'invalid values' : 'an invalid value'}`
+
+        context.errors.push({
+          text,
+          name: field.name,
+          href: `#${field.name}`,
+          path: [`#${field.name}`]
+        })
+      }
+
+      return isInvalid
     }
   }
 
