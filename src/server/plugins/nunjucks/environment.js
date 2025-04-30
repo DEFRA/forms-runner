7a2f6825f3c001b31081b866853706bf25c830a5
Alex Luckett <alexgluckett@gmail.com>
2025-04-30 15:57:42 +0100

    Remove nunjucks evaluation

--- src/server/plugins/nunjucks/environment.js
+++ src/server/plugins/nunjucks/environment.js
@@ -1,7 +1,5 @@
 import { dirname, join } from 'node:path'
 
-import { evaluateTemplate } from '@defra/forms-engine-plugin/engine/helpers.js'
-import { ComponentType } from '@defra/forms-model'
 import nunjucks from 'nunjucks'
 import resolvePkg from 'resolve'
 
@@ -22,87 +20,3 @@ export const environment = nunjucks.configure(
     noCache: config.get('isDevelopment')
   }
 )
-
-/**
- * @this {NunjucksContext}
- * @param {FormSubmissionError[]} errors
- */
-function checkErrorTemplates(errors) {
-  const { context } = this.ctx
-
-  if (!context) {
-    return errors
-  }
-
-  errors.forEach((error) => {
-    error.text = evaluateTemplate(error.text, context)
-  })
-
-  return errors
-}
-
-environment.addGlobal('checkErrorTemplates', checkErrorTemplates)
-
-/**
- * @this {NunjucksContext}
- * @param {ComponentViewModel} component
- */
-function checkComponentTemplates(component) {
-  const { context } = this.ctx
-
-  if (!context) {
-    return component
-  }
-
-  if (component.isFormComponent) {
-    // Evaluate label/legend text
-    if (component.model.fieldset?.legend?.text) {
-      const legend = component.model.fieldset.legend
-
-      legend.text = evaluateTemplate(legend.text, context)
-    } else if (component.model.label?.text) {
-      const label = component.model.label
-
-      label.text = evaluateTemplate(label.text, context)
-    } else {
-      // No template evaluation needed for other component types
-    }
-
-    // Evaluate error message
-    if (component.model.errorMessage?.text) {
-      const message = component.model.errorMessage
-
-      message.text = evaluateTemplate(message.text, context)
-    }
-  } else if (component.type === ComponentType.Html) {
-    const content = component.model.content
-
-    if (typeof content === 'string') {
-      component.model.content = evaluateTemplate(content, context)
-    }
-  } else {
-    // No template evaluation needed for other component types
-  }
-
-  return component
-}
-
-environment.addGlobal('checkComponentTemplates', checkComponentTemplates)
-
-/**
- * @this {NunjucksContext}
- * @param {string} template
- */
-function evaluate(template) {
-  const { context } = this.ctx
-
-  return context ? evaluateTemplate(template, context) : template
-}
-
-environment.addGlobal('evaluate', evaluate)
-
-/**
- * @import { NunjucksContext } from '~/src/server/plugins/nunjucks/types.js'
- * @import { FormSubmissionError } from '~/src/server/plugins/engine/types.js'
- * @import { ComponentViewModel } from '~/src/server/plugins/engine/components/types.js'
- */
