0535dc78cc768602cec3838d3be4b0a4e09d60b7
Alex Luckett <alexgluckett@gmail.com>
2025-05-21 10:45:18 +0100

    Fix broken mock - incorrect path

--- test/form/feedback.test.js
+++ test/form/feedback.test.js
@@ -1,16 +1,15 @@
 import { join } from 'node:path'
 
-import { getFormMetadata } from '@defra/forms-engine-plugin/engine/services/formsService.js'
-
 import { FORM_PREFIX } from '~/src/server/constants.js'
 import { createServer } from '~/src/server/index.js'
+import { getFormMetadata } from '~/src/server/services/formsService.js'
 import * as fixtures from '~/test/fixtures/index.js'
 import { renderResponse } from '~/test/helpers/component-helpers.js'
 
 const { FEEDBACK_LINK } = process.env
 const basePath = `${FORM_PREFIX}/feedback`
 
-jest.mock('@defra/forms-engine-plugin/engine/services/formsService.js')
+jest.mock('~/src/server/services/formsService.js')
 
 describe('Feedback link', () => {
   /** @type {Server} */
