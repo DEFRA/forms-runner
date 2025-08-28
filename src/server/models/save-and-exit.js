7ff6ff445eae4f8b4d7febfc6e1b50153372cef9
David Stone <davidjamesstone@gmail.com>
2025-08-28 16:51:05 +0100

    Update jsdoc function description

--- src/server/models/save-and-exit.js
+++ src/server/models/save-and-exit.js
@@ -184,7 +184,7 @@ export function getFlashKey(params) {
 }
 
 /**
- *
+ * The save and exit form view model
  * @param {SaveAndExitParams} params
  * @param {SaveAndExitPayload} [payload]
  * @param {Error} [err]
