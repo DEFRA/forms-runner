c0d86f1a36cf2a3a71c91a96468fecffc6922b77
Jez Barnsley <jbarnsley.github@gmail.com>
2025-11-12 11:17:57 +0000

    Adde journey test

---
+++ test/form/definitions/confirmation-email.js
@@ -0,0 +1,49 @@
+import {
+  ComponentType,
+  ControllerPath,
+  ControllerType,
+  Engine,
+  SchemaVersion
+} from '@defra/forms-model'
+
+export default /** @satisfies {FormDefinition} */ ({
+  name: 'Confirmation email',
+  startPage: '/licence',
+  engine: Engine.V2,
+  schema: SchemaVersion.V2,
+  pages: /** @type {const} */ ([
+    {
+      title: "What's your name?",
+      path: '/full-name',
+      components: [
+        {
+          schema: {
+            max: 70
+          },
+          options: {},
+          type: ComponentType.TextField,
+          name: 'fullName',
+          title: "What's your name?"
+        }
+      ],
+      next: [
+        {
+          path: '/summary'
+        }
+      ]
+    },
+    {
+      path: ControllerPath.Summary,
+      controller: ControllerType.SummaryWithConfirmationEmail,
+      title: 'Summary'
+    }
+  ]),
+  sections: [],
+  conditions: [],
+  lists: [],
+  outputEmail: 'enrique.chase@defra.gov.uk'
+})
+
+/**
+ * @import { FormDefinition } from '@defra/forms-model'
+ */
