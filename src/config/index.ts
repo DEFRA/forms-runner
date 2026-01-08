006e0f3de7a156ba807b84e0d04d4a84662f6842
David Stone <davidjamesstone@gmail.com>
2026-01-08 13:03:28 +0000

    Update OS API KEY config to inform it is used for both postcode lookup and maps

--- src/config/index.ts
+++ src/config/index.ts
@@ -301,7 +301,7 @@ export const config = convict({
   },
 
   ordnanceSurveyApiKey: {
-    doc: 'The ordnance survey api key used by the postcode lookup plugin',
+    doc: 'The ordnance survey api key used by the postcode lookup and maps plugin',
     format: String,
     nullable: true,
     default: undefined,
