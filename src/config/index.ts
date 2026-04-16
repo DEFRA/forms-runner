f2519f768151cf37fa08cdd5405907fa2997805a
Alex Luckett <alexgluckett@gmail.com>
2026-04-16 11:12:47 +0100

    remove safelist

--- src/config/index.ts
+++ src/config/index.ts
@@ -266,12 +266,6 @@ export const config = convict({
     }
   },
 
-  // safelist: {
-  //   format: Array,
-  //   default: null,
-  //   env: 'SAFELIST'
-  // } as SchemaObj<string[]>,
-
   stagingPrefix: {
     doc: 'Prefix for staging files in S3',
     format: String,
